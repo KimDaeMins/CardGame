9d19fa4d75e153bbc7c992ff7d23ccc13512ba38
KimDaeMins <eoals81712@gmail.com>
2023-08-08 20:52:05 +0900

    최종복구본

---
+++ findRtan/Assets/Scripts/audioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public AudioClip bgMusic;
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
