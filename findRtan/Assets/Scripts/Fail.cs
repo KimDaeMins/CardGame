adf3cbe9f705ccb0f22d0af51df2faa696099c06
toadsam <toadsam@naver.com>
2023-08-10 12:06:47 +0900

    fail 점점 투명화 수정

--- findRtan/Assets/Scripts/Fail.cs
+++ findRtan/Assets/Scripts/Fail.cs
@@ -9,9 +9,13 @@ public class Fail : MonoBehaviour
     public float upSizeTime;
     public float scalespeed;
     public float destroytime;
+    Color alpha;
+    public float alphaspeed;
+    
     // Start is called before the first frame update
     void Start()
     {
+        alpha = GetComponent<SpriteRenderer>().color; 
         Invoke("DestroyObject", destroytime);
     }
 
@@ -19,6 +23,8 @@ public class Fail : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
+        alpha.a = Mathf.Lerp(alpha.a, 0, Time.deltaTime * alphaspeed);
+        GetComponent<SpriteRenderer>().color = alpha;
         if (time <= upSizeTime)
         {
             transform.localScale = new Vector3(0.5f,0.5f,1) * (1+ size * time);
